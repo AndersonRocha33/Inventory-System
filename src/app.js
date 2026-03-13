275f1a1b0261795a3725270bd1717abaf9dbbb81
AndersonRocha33 <anderson_paracuru@hotmail.com>
2026-03-12 23:34:17 -0300

    ajusta cors para frontend

--- src/app.js
+++ src/app.js
@@ -10,7 +10,12 @@ const reportRoutes = require("./routes/reportRoutes")
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}))
+
 app.use(express.json())
 
 app.use("/inventory", inventoryRoutes)
